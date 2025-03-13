efdb7ad8b569591bf09dacd559ef7bec6c4c942a
KeithKris <123614690+KeithKris@users.noreply.github.com>
2025-03-12 22:53:08 -0400

    Add files via upload
    first test of imagewriter control codes

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from PIL import Image
+from imagewriter import Imagewriter
 
 palette = [
         255, 255, 255, #white
         0, 255, 255, #cyan
         255, 0, 255, #magenta
         255, 255, 0, #yellow
+        255, 0, 0, #red
+        0, 255, 0, #green
         0, 0, 0, #black
     ]
 
@@ -13,18 +16,26 @@ full_palette = palette * (256 // len(palette))
 
 print_resolution=(720,576)
 
+IW = Imagewriter()
+
 def main():
     # Open the image file
     image = Image.open("in.jpg")
     output = dither_and_resize(image, full_palette, print_resolution)
+    output.save("out.png")
+    printer_bytes = IW.print_image(output)
+    
+    with open("printdata.bin", "wb") as file:
+        file.write(printer_bytes)
+    
 
 def dither_and_resize(image, palette, resolution):
     p_img = Image.new('P', (512, 512))
     p_img.putpalette(full_palette)
 
-    conv = image.quantize(palette=p_img)
+    return image.resize(print_resolution).quantize(palette=p_img).convert('RGB')
 
-    return conv.resize(print_resolution).convert('RGB')
+    #return conv.resize(print_resolution).convert('RGB')
 
 if __name__ == '__main__':
     main()
